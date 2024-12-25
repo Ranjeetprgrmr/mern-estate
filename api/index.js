fca7ecb5d1a59afd10b8f7bda65556f6259ed528
Ranjeet Singh Tharu <67905362+Ranjeetprgrmr@users.noreply.github.com>
2024-12-25 21:27:25 +0545

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
 
 dotenv.config();
 
@@ -15,3 +16,12 @@ const app = express();
 app.listen(3000, () => {
     console.log('Server listening on port 3000!!!');
 })
+
+// api route
+// app.get('/test', (req, res) => {
+//     // res.send('Hello World!');
+//     res.json({
+//         message: 'hello world'
+//     })
+// })
+app.use('/api/user', userRouter)
