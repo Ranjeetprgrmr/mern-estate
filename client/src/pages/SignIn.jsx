8e300d24a09869e27e73461186000409c8461390
Ranjeet Singh Tharu <67905362+Ranjeetprgrmr@users.noreply.github.com>
2024-12-28 12:29:02 +0545

    add redux persist

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -6,7 +6,7 @@ import {
   signInSuccess,
   signInFailure,
 } from "../redux/user/userSlice";
-import './../index.css';
+
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
