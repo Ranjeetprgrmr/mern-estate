6fa0ef55cf4d008295b60618cba19c74dd09c8b3
Ranjeet Singh Tharu <67905362+Ranjeetprgrmr@users.noreply.github.com>
2025-01-06 20:14:21 +0545

    complete update listing api route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createListing, deleteListing } from '../controllers/listing.controller.js';
+import { createListing, deleteListing, updateListing} from '../controllers/listing.controller.js';
 import { verifyToken } from '../utils/verifyToken.js';
 import { upload } from '../utils/upload.js';
 
@@ -7,5 +7,6 @@ const router = express.Router();
 
 router.post('/create', verifyToken,  upload().array('imageUrls', 6), createListing);
 router.delete('/delete/:id', verifyToken, deleteListing);
+router.post('/update/:id', verifyToken, upload().array('imageUrls', 6), updateListing);
 
 export default router;
